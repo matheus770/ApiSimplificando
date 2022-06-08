5f924a12413035f6b91e256985f8b184c6de4bf8
NecoMatheus <matheus770@users.noreply.github.com>
2022-06-07 21:07:26 -0300

    endpoints so de get

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import json
-from flask import Flask, Response, request
+from flask import Flask, Response, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///simplificando.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Tutorials(db.Model):
-   title = db.Column(db.String(150), nullable=False)
+   title = db.Column(db.String(150), nullable=False) 
    video_description = db.Column(db.String(500), nullable=False)
    video_key = db.Column(db.String(80), nullable=False)
    video_id = db.Column(db.String(80), nullable=False, primary_key=True,  autoincrement=False)
@@ -16,48 +17,27 @@ class Tutorials(db.Model):
         return{"title": self.title, "video_description": self.video_description, "video_key": self.video_key, "video_id": self.video_id}
 
 @app.route('/')
-def index():
+def homepage():
+    return("Simplificando")
+    
+@app.route('/v1/tutorials/', methods=['GET'])
+def get_tutorial():
     try: 
         tutorials = Tutorials.query.all()
         tutorials_json = [tutorial.to_Json() for tutorial in tutorials]
         return rtn_response(200, "tutorials", tutorials_json)
     except Exception as e:
-        return rtn_response(400, "tutorials", e, "Não foi possivel localizar os Tutoriais") 
+        return rtn_response(404, "tutorials", e, "Não foi possivel localizar os Tutoriais") 
 
-@app.route('/new', methods=['POST'])
-def new():
+@app.route('/v1/tutorials/<title>', methods=['GET'])
+def search_tutorial(title):
     try:
-        body = request.get_json()
-        tutorial = Tutorials(title=body['title'], video_description=body['video_description'], video_key=body['video_key'],  video_id=body['video_id'])
-        db.session.add(tutorial)
-        db.session.commit()
-        return rtn_response(201, "tutorials", "Tutorial criado com sucesso")
+        tutorials = Tutorials.query.all()
+        tutorials_json = [tutorial.to_Json() for tutorial in tutorials]
+        tutorials_search = [tutorial for tutorial in tutorials_json if title in tutorial['title']]
+        return rtn_response(201, "tutorials", tutorials_search)
     except Exception as e:
-        return rtn_response(400, "tutorials", e, "Não foi possivel Criar o Tutorial") 
-
-
-# @app.route('/<id>')
-# def musica_pelo_id(id):
-#    musica = Musica.query.get(id)
-#    return render_template('index.html', musica=musica)
-
-# @app.route('/edit/<id>', methods=['GET', 'POST'])
-# def edit(id):
-#    musica = Musica.query.get(id)
-#    if request.method == "POST":
-#       musica.nome = request.form['nome']
-#       musica.artista = request.form['artista']
-#       musica.link = request.form['link']
-#       db.session.commit()
-#       return redirect('/#playlist')
-#    return render_template('edit.html', musica=musica)
-
-# @app.route('/delete/<id>')
-# def delete(id):
-#    musica = Musica.query.get(id)
-#    db.session.delete(musica)
-#    db.session.commit()
-#    return redirect('/#playlist')
+        return rtn_response(404, "tutorials", e, "Não foi possivel localizar o Tutorial") 
 
 def rtn_response(status, context_Name, context, mensagem=False):
     body = {}
